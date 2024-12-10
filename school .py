a2427bcbf8736a8427318c93c4daaf780c8699d6
dlarkdhs <imgaon1234@gmail.com>
2024-12-10 22:17:23 +0900

    기말시험 힌트

--- school .py
+++ school .py
@@ -1,17 +1,76 @@
 import sys
 input = sys.stdin.readline
 
-# 문제 1
-email1 = "cherry1004@gmail.com"
-email2 = "cherry01@gmail.com"
-email3 = "cherry0@gmail.com"
-print(email1[0:10],email2[0:8],email3[0:7],sep = '\n')
+# 3번.
+numbers=[1,2,3,4]
+print(numbers[2])
+# 3
 
-# 문제 2
-site1 = "www.nave.com"
-site2 = "www.daum.net"
-site3 = "www.google.com"
-domain1 = site1.split('.')[1]
-domain2 = site2.split('.')[1]
-domain3 = site3.split('.')[1]
-print(domain1,domain2,domain3,sep = '\n')
+# 14번.
+for i in range(3):
+    print(i)
+# 0 /n 1 /n 2
+
+# 21번.
+if 5>3: print('참')
+else: print('거짓')
+# 참
+
+# 24번.
+nums = [10,20,30]
+nums.append(40)
+print(nums)
+# [10, 20, 30, 40]
+
+# 25번.
+lst=[1,2,3,4,5]
+print(lst[1:4])
+# [2, 3, 4]
+
+# 36번.
+for i in range(5):
+    if i == 3:
+        continue
+    print(i)
+# 0, 1, 2, 4
+
+# 40번.
+def add(a, b):
+    return a + b
+print(add(2, 3))
+# 5
+
+# 41번.
+def subtract(a, b):
+    return a - b
+result = subtract(b=10, a=20)
+print(result)
+# 10
+
+# 43번.
+def repeat(word, times):
+    return word * times
+print(repeat("Hi", 3))
+# HiHiHi
+
+# 44번.
+def print_name(name="John"):
+    print(name)
+print_name("Alice")
+# Alice
+
+# 45번.
+def calc_square(x):
+    return x ** 2
+print(calc_square(4))
+# 16
+
+# <손코딩 문제(부분 코딩 또는 풀코딩)>
+# 1. 변수 s1에 "대한", s2에 "민국" 문자열을
+# 대입한 후, 다음 형태로 출력해보자.
+
+s1 = "대한"
+s2 = "민국"
+print(s1+s2)
+print(s1,s2)
+print(s1+s2,"만세")
