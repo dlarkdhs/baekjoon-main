ceae93111a0a900039e457797ad8528b9c09520f
dlarkdhs <imgaon1234@gmail.com>
2025-02-23 18:14:41 +0900

    백준 11060 (S2)

---
+++ silver/S2/점프 점프.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int,input().split()))
+dp = [n+1]*n
+dp[0] = 0
+
+for i in range(n):
+    for j in range(1,a[i]+1):
+        if i+j >= n:
+            break
+        dp[i+j] = min(dp[i+j],dp[i]+1)
+if dp[n-1] > n:
+    print(-1)
+else:
+    print(dp[n-1])
